be76c6e0e8a83fa4b7a62beedd02b751f913784c
Kiril <kiril_zafirov@yahoo.com>
2020-08-18 16:35:56 +0200

    calculation

--- src/app/features/calculations/calculations.component.ts
+++ src/app/features/calculations/calculations.component.ts
@@ -1,6 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
-
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+export interface EntryProduct { 
+  subject: string;
+  offeredGoods: string;
+  production:string;
+  priceBeforeTax: number;
+  measureAndAmount: string;
+  ddvRate: number;
+  totalRequired: number;
+}
+export interface Result {
+  priceBeforeDDV: number;
+  priceAfterDDV: number;
+}
+export interface SelectOption {
+  display: string;
+  value: string | number;
+}
 @Component({
   selector: 'app-calculations',
   templateUrl: './calculations.component.html',
@@ -10,20 +28,73 @@ export class CalculationsComponent implements OnInit {
 
   dynamicForm: FormGroup; 
 
+  measureAndAmount: SelectOption[] = [
+    {
+      display: 'Килограм',
+      value: 'kg'
+    },
+    {
+      display: 'Литар',
+      value: 'lt'
+    },
+    {
+      display: 'Број',
+      value: 'br'
+    }
+  ];
+  ddvRate: SelectOption[] = [
+    {
+      display: '5%',
+      value: 0.05
+    },
+    {
+      display: '18%',
+      value: 0.18
+    }
+  ]
   constructor(private formBuilder: FormBuilder) { 
     this.dynamicForm = this.formBuilder.group({
       items: new FormArray([this.getNewItemControl()])
   });
   }
    
+  result$: Observable<Result>;
   ngOnInit() {
-      
+    this. result$ = this.dynamicForm.valueChanges.pipe(
+      map((listOfProducts: EntryProduct[]) => this.calculateResult(listOfProducts))
+    );
   }
  
-  
+
+  calculateResult(listOfProducts: EntryProduct[]):Result {
+   
+    const summary = listOfProducts.reduce((acc, value, index) => {
+      acc += +value;
+      return 0
+    }, 0);
+
+    return {
+      priceBeforeDDV: 0,
+      priceAfterDDV: 0
+    }
+  }
+
+  calculateSingle(product: EntryProduct):Result {
+    const priceBeforeDDVForAll = product.priceBeforeTax * product.totalRequired;
+    const taxDiff = priceBeforeDDVForAll * product.ddvRate;
+    return {
+      priceBeforeDDV: priceBeforeDDVForAll,
+      priceAfterDDV: product.priceBeforeTax + taxDiff
+    }
+  }
+
   getNewItemControl() {
-    return this.formBuilder.group({
-      name: ['', Validators.required],
+    return this.formBuilder.group({ 
+      subject: [''],
+      priceBeforeTax: [null, Validators.required],
+      measureAndAmount: ['kg' , Validators.required],
+      ddvRate: [0.18 , Validators.required],
+      totalRequired: [null , Validators.required],
     });
   }
 
@@ -52,10 +123,5 @@ export class CalculationsComponent implements OnInit {
       // reset whole form back to initial state 
       this.dynamicForm.reset();
       this.items.clear();
-  }
-
-  onClear() {
-      // clear errors and reset ticket fields 
-      this.items.reset();
   }
 }
